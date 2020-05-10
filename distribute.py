f2393bb7bff42646be8df05700c519085d7ec2eb
yangping <1477311729@qq.com>
2020-05-10 21:17:19 +0800

    fix

--- distribute.py
+++ distribute.py
@@ -74,13 +74,7 @@ def main(_):
       tf.summary.scalar('cost', loss_value)
       summary_op = tf.summary.merge_all()
  
-    sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
-                            logdir="./checkpoint/",
-                            init_op=init_op,
-                            summary_op=None,
-                            saver=saver,
-                            global_step=global_step,
-                            save_model_secs=60)
+    sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),logdir="./checkpoint/",init_op=init_op,summary_op=None, saver=saver,global_step=global_step,save_model_secs=60)
 
     with sv.prepare_or_wait_for_session(server.target) as sess:
       # 如果是同步模式
